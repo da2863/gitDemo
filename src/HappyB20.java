028f147328f6a8bd3dd4bbc85fef3289fa22f0a7
Daulet <aliev.daulet1@gmail.com>
2020-08-01 11:53:43 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy");
+
+        System.out.println("Git is no joke ");
     }
 }
--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,5 +3,7 @@ public class HelloB20 {
     public static void main(String[] args) {
 
         System.out.println("HelloB20, it is my pleasure");
+
+
     }
 }
